f3ea71b4d8f96f1fbf3b37aedebc5342b948e523
ganezasan <takayuki@circleci.com>
2022-09-04 13:40:32 +0900

    update Button and snapshot data

--- src/08_ui/Button.test.tsx
+++ src/08_ui/Button.test.tsx
@@ -1,6 +1,6 @@
 import renderer from 'react-test-renderer'
 import { render, fireEvent, screen } from '@testing-library/react';
-import Button from './Button'
+import { Button } from './Button'
 
 describe('Button', () => {
   it('renders correctly with react-test-renderer', () => {
@@ -8,15 +8,15 @@ describe('Button', () => {
     expect(button.toJSON()).toMatchSnapshot()
   })
   it('changes the text after click', () => {
-    const button = render(<Button />);
-    fireEvent.click(button.getByText('ON'));
-    expect(button.getByText(/OFF/i)).toBeTruthy();
-    fireEvent.click(button.getByText('OFF'));
-    expect(button.getByText(/ON/i)).toBeTruthy();
+    const button = render(<Button />)
+    fireEvent.click(button.getByText('ON'))
+    expect(button.getByText(/OFF/i)).toBeTruthy()
+    fireEvent.click(button.getByText('OFF'))
+    expect(button.getByText(/ON/i)).toBeTruthy()
   })
 
-  it('debug screen', () => {
-    const button = render(<Button />);
-    screen.debug();
-  })
+  // it('debug screen', () => {
+  //   const button = render(<Button />)
+  //   screen.debug()
+  // })
 })

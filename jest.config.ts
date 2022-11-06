97039ba7aafc6e0463ea401a1f0fe14ed772107d
ganezasan <takayuki@circleci.com>
2022-11-06 16:32:13 +0900

    enable code coverage

--- jest.config.ts
+++ jest.config.ts
@@ -11,5 +11,8 @@ export default {
   reporters: [
     "default",
     ['jest-junit', { outputDirectory: 'reports', outputName: 'jest-report.xml' }],
-  ]
+  ],
+  collectCoverage: true,
+  coverageReporters: ["text-summary", "html"],
+  coverageDirectory: "reports/coverage",
 }

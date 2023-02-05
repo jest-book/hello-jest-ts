402363b7199f0cd6e23fa24bdd5454925bb3a568
ganezasan <istaka12@gmail.com>
2023-02-05 23:53:40 +0900

    update src/chapter2/group/concurrent.test.ts

--- src/chapter2/group/concurrent.test.ts
+++ src/chapter2/group/concurrent.test.ts
@@ -2,16 +2,13 @@
 const fetchData = () =>
   new Promise(resolve => setTimeout(resolve, 1000, 'lemon'))
 
-describe.skip('concurrent tests', () => {
-  // n回数fetchDataを実行しテストする
-  const runConcurrentTests = n => {
-    Array.from(new Array(n).keys()).map(i => {
-      it.concurrent(`test-${i}: return lemon`, async () => {
-        await expect(fetchData()).resolves.toBe('lemon')
-      })
-    })
-  }
-
-  // 100回呼び出す
-  runConcurrentTests(100)
+// 100回数fetchData関数をテスト
+test.concurrent.each(
+  Array.from(new Array(100).keys()).map(n => ({
+    n,
+    expected: 'lemon',
+  })),
+)('concurrent tests $n', async ({ n, expected }) => {
+  console.log(n)
+  await expect(fetchData()).resolves.toBe(expected)
 })

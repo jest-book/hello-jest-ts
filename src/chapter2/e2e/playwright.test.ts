96cd46a4d61ff5ae6c4c5122975c6234864a77f9
ganezasan <istaka12@gmail.com>
2023-02-09 02:31:35 +0900

    update playwright.test.ts

--- src/chapter2/e2e/playwright.test.ts
+++ src/chapter2/e2e/playwright.test.ts
@@ -1,6 +1,4 @@
-jest.setTimeout(10000)
-
-import { Browser, Page, chromium, firefox, webkit } from 'playwright'
+import { Browser, chromium, firefox, webkit } from 'playwright'
 
 describe.each([
   { browserType: chromium, browserName: 'chromium' },
@@ -8,28 +6,22 @@ describe.each([
   { browserType: webkit, browserName: 'webkit' },
 ])('e2e test with playwright and $browserName', ({ browserType }) => {
   let browser: Browser
-  let page: Page
 
   beforeAll(async () => {
     browser = await browserType.launch() // ブラウザの起動
-    // browser = await browserType.launch({ headless: false }) // 実際にブラウザの挙動を見たい場合はheadlessモードを無効化する
+    // 実際にブラウザの挙動を見たい場合はheadlessモードを無効化する
+    // browser = await browserType.launch({ headless: false })
   })
 
   afterAll(async () => {
     await browser.close() //ブラウザの終了
   })
 
-  beforeEach(async () => {
-    page = await browser.newPage({
-      recordVideo: {
-        dir: 'videos',
-      },
-    }) // 新しいページを立ち上げる
-  })
-
   it('a search keyword will be on the page title in google.com', async () => {
+    // 新しいページを立ち上げる
+    const page = await browser.newPage()
     await page.goto('https://www.google.com/ncr')
-    // 検索ボックスの要素を探し、puppeteerを入力しエンターキーをクリック
+    // 検索ボックスの要素を探し、playwrightを入力しエンターキーをクリック
     await page.type('input[name="q"]', 'playwright')
     await page.keyboard.press('Enter')
 
@@ -39,10 +31,14 @@ describe.each([
       waitUntil: 'domcontentloaded',
     })
     expect(await page.title()).toBe('playwright - Google Search')
+    // ページを終了する
+    await page.close()
   })
 
-  it('should work with Code generator testscases', async () => {
-    // Go to https://www.google.com/ncr
+  it('should work with Code generator test code', async () => {
+    // 新しいページを立ち上げる
+    const page = await browser.newPage()
+    // Go to https://www.google.com/ コードジェネレーター上ではリダイレクトされ、`ncr`のパスが消えているため手動で追加
     await page.goto('https://www.google.com/ncr')
     // Click [aria-label="Search"]
     await page.locator('[aria-label="Search"]').click()
@@ -51,11 +47,14 @@ describe.each([
     // Press Enter
     await page.locator('[aria-label="Search"]').press('Enter')
 
+    // アサーションはURLが利用されているが、検索毎にURLのパラメーターは変わるため、ページタイトルを利用
     // ページのタイトルが`playwright - Google Search`に切り替わるまで待つ
     await page.waitForNavigation({
       timeout: 2000,
       waitUntil: 'domcontentloaded',
     })
     expect(await page.title()).toBe('playwright - Google Search')
+    // ページを終了する
+    await page.close()
   })
 })

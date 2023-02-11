80ab4b463415ccf836dd2f30d0fe053097ebd034
ganezasan <istaka12@gmail.com>
2023-02-11 10:21:44 +0900

    fixed some package versions

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,12 @@
 export default {
   preset: 'ts-jest/presets/js-with-ts-esm',
-  globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.json"
-    }
-  },
   moduleNameMapper: {
     "\\.(css|less|scss)$": "identity-obj-proxy"
   },
-  reporters: [
-    "default",
-    ['jest-junit', { outputDirectory: 'reports', outputName: 'jest-report.xml' }],
-  ],
+//  reporters: [
+//    "default",
+//    ['jest-junit', { outputDirectory: 'reports', outputName: 'jest-report.xml' }],
+//  ],
   collectCoverage: true,
   coverageReporters: ["text-summary", "html"],
   coverageDirectory: "reports/coverage",

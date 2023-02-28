2706e8c8fd3f1d672fc46fc72960b515fdfa94ee
ganezasan <istaka12@gmail.com>
2023-02-28 09:53:23 +0900

    fix prettier issue

--- src/chapter2/assertion/equalityOperators.js
+++ src/chapter2/assertion/equalityOperators.js
@@ -1,12 +1,12 @@
 /* eslint-disable */
 
 // 二重等号を利用した比較では暗黙的な型変換が発生する
-console.log("2" == 2) // true
+console.log('2' == 2) // true
 console.log(true == 1) // true
 
 // 三重等号を利用した比較では暗黙的な型変換は発生しない
 // 型が異なるため、等しくないと評価される
-console.log("2" === 2) // false
+console.log('2' === 2) // false
 console.log(true === 1) // false
 
 // Object.isはNaNと0を特別扱いしない
@@ -21,4 +21,3 @@ console.log(+0 == -0) // true
 // Object.is
 console.log(Object.is(NaN, NaN)) // true
 console.log(Object.is(+0, -0)) // false
-

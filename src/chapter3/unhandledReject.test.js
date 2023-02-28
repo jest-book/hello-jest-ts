c361a268cd6eda86df7921ac287760d85f2d2a62
ganezasan <istaka12@gmail.com>
2023-02-28 09:18:34 +0900

    意図したエラーを出力しているため、skipを追加

--- src/chapter3/unhandledReject.test.js
+++ src/chapter3/unhandledReject.test.js
@@ -10,6 +10,8 @@ const magicalFunction = () => {
   return response ? true : false
 }
 
-test('magicalFunction returns true if fetchDataReject returns any object', () => {
+// Promiseがキャッチできないため、テストケースは成功となるが、コンソールにはエラーが表示される
+// 実際に試す場合は test.skip の .skipを削除
+test.skip('magicalFunction returns true if fetchDataReject returns any object', () => {
   expect(magicalFunction()).toBe(true)
 })

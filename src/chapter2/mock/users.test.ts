3f68754a98b3c6452b99514646dd466da02339eb
katayama8000 <63860688+katayama8000@users.noreply.github.com>
2023-04-25 23:16:54 +0900

    fix: typo
    書籍読んでて気がつきました。

--- src/chapter2/mock/users.test.ts
+++ src/chapter2/mock/users.test.ts
@@ -7,7 +7,7 @@ test('should fetch all users', async () => {
   const users = [{ name: 'Bob' }]
   const resp = { data: users }
 
-  ;(axios as jest.Mocked<typeof axios>).get.mockResolvedValue(resp)
+  (axios as jest.Mocked<typeof axios>).get.mockResolvedValue(resp)
   // axios.get.mockImplementation(() => Promise.resolve(resp)) // 上記のmockResolvedValueと同じ設定
 
   await expect(Users.search()).resolves.toEqual(users)
